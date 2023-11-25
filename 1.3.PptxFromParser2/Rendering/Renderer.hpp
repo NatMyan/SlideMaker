4969e052e83ce38fa11653339551096c00870a84
Natalie Manukyan <nataliemanukyan2021@gmail.com>
2023-11-25 14:15:11 +0400

    update more stuff

---
+++ 1.3.PptxFromParser2/Rendering/Renderer.hpp
@@ -0,0 +1,9 @@
+#ifndef RENDERER_HPP
+#define RENDERER_HPP
+
+class Renderer {
+    public:
+        virtual void execute() = 0;
+};
+
+#endif // RENDERER_HPP
---
+++ 1.3.PptxFromParser2/Serializing/Serializer.hpp
@@ -0,0 +1,9 @@
+#ifndef SERIALIZER_HPP
+#define SERIALIZER_HPP
+
+class Serializer {
+    public:
+        virtual void execute() = 0;
+};
+
+#endif // SERIALIZER_HPP
