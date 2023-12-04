3b43d47a15c5a6c0686c17d027b8afd0700e78f2
Natalie Manukyan <nataliemanukyan2021@gmail.com>
2023-12-04 20:15:06 +0400

    update

--- Abandoned/Data/Document.hpp
+++ Abandoned/Data/Document.hpp
@@ -12,10 +12,10 @@ class Document {
         Document (int slideNum);
 
     public:
-        DocumentType<std::shared_ptr<Slide> > getSlides();
-        void addtoDocument(std::shared_ptr<Slide> slidePtr);
-        void removeFromDocument(Idx idx);
-        std::weak_ptr<Slide> getSlide(Idx idx);
+        //DocumentType<std::shared_ptr<Slide> > getSlides();
+        Idx addSlide(std::shared_ptr<Slide> slidePtr);
+        void removeSlide(Idx idx);
+        std::shared_ptr<Slide> getSlide(Idx idx);
         DocumentIterator begin();
         DocumentIterator end();
 
--- Abandoned/Data/Slide.hpp
+++ Abandoned/Data/Slide.hpp
@@ -5,19 +5,19 @@
 
 class Slide {
     public:
-        using SlideIterator = SlideType<std::shared_ptr<Item> >::iterator;
+        using ItemIterator = SlideType<std::shared_ptr<Item> >::iterator;
 
     public:
         Slide() = default;
     
     public:
-        SlideType<std::shared_ptr<Item> > getSlide();         
-        void addtoSlide(std::shared_ptr<Item> itemPtr);
-        void removeFromSlide(ID id);
+        //SlideType<std::shared_ptr<Item> > getSlide();         
+        void addItem(std::shared_ptr<Item> itemPtr);
+        void removeItem(ID id);
         std::shared_ptr<Item> getItem(ID id);
         // void setItem(Type type, ID id, Position pos, Attributes attributes);
-        SlideIterator begin();
-        SlideIterator end();
+        ItemIterator begin();
+        ItemIterator end();
 
     private:
         SlideType<std::shared_ptr<Item> > slide_; 
