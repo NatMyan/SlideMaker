e27497914cd049d33110c9e2cf40305b709d5c6f
Natalie Manukyan <nataliemanukyan2021@gmail.com>
2023-12-05 00:31:39 +0400

    update

--- 8.HopefullyFinal/CLI/Parser8/SemanticAnalyzer.hpp
+++ 8.HopefullyFinal/CLI/Parser8/SemanticAnalyzer.hpp
@@ -1,6 +1,8 @@
 #ifndef SEMANTIC_ANALYZER_HPP
 #define SEMANTIC_ANALYZER_HPP
 
+#include "../../definitions.hpp"
+
 #include <istream>
 
 ///NOTE: separating into classes, cause I don't want to put every function into 1 class
--- 8.HopefullyFinal/CLI/Parser8/SyntaxAnalyzer.hpp
+++ 8.HopefullyFinal/CLI/Parser8/SyntaxAnalyzer.hpp
@@ -1,6 +1,8 @@
 #ifndef SYNTAX_ANALYZER_HPP
 #define SYNTAX_ANALYZER_HPP
 
+#include "../../definitions.hpp"
+
 #include <istream>
 
 ///NOTE: separating into classes, cause I don't want to put every function into 1 class
