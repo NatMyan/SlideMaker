ed12e51e3949a905bba36f1b944c5a9f6eabf8c3
Natalie Manukyan <nataliemanukyan2021@gmail.com>
2023-12-07 01:00:49 +0400

    update

---
+++ 8.HopefullyFinal/Rendering/RectangleShape.hpp
@@ -0,0 +1,1 @@
+#ifndef RECT_SHAPE_HPP
--- 8.HopefullyFinal/Rendering/ShapeBase.cpp
+++ 8.HopefullyFinal/Rendering/ShapeBase.cpp
@@ -1,2 +1,9 @@
 #include "ShapeBase.hpp"
 
+std::unique_ptr<IShape> ShapeBase::clone() {
+    return std::make_unique<ShapeBase>(*this); // deep copy
+}
+
+void ShapeBase::display(std::shared_ptr<ItemBase> item, std::ostream& output) {
+    output << item stuff;
+}
