b1b2f56f17492aa2d6f01b550a8028b6bb7840bf
Natalie Manukyan <nataliemanukyan2021@gmail.com>
2023-12-09 00:09:13 +0400

    update

--- 8.HopefullyFinal/Director/ActionContainer.hpp
+++ 8.HopefullyFinal/Director/ActionContainer.hpp
@@ -8,14 +8,17 @@
 ///NOTE: to replace stack and an iterator on it, to simplify testing and coding (in my opinion and understanding)
 class ActionContainer { 
     public:
-        void addRedoableAction(std::shared_ptr<IAction> action);
-        void addUndoableAction(std::shared_ptr<IAction> action);
+        void addAction(std::shared_ptr<IAction> action);
 
         void popFrontRedoable();
         void popBackRedoable();
 
         void popFrontUndoable();
         void popBackRedoable();
+    
+    private:
+        void addRedoableAction(std::shared_ptr<IAction> action);
+        void addUndoableAction(std::shared_ptr<IAction> action);
 
     private:
         std::list<std::shared_ptr<IAction> > undoables_;
