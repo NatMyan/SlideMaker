78dea73740fb1e977e5ca32606dcb8b3c21528db
Natalie Manukyan <nataliemanukyan2021@gmail.com>
2023-12-10 15:52:00 +0400

    update

--- 8.HopefullyFinal/CLI/Commands/SaveCommand.cpp
+++ 8.HopefullyFinal/CLI/Commands/SaveCommand.cpp
@@ -1,4 +1,8 @@
 #include "SaveCommand.hpp"
+#include "../../Serializing/Serializer.hpp"
+#include "../../Application.hpp"
+
+#include <fstream>
 
 SaveCommand::SaveCommand(const Map& info) :
     infoMap_(info)
@@ -6,12 +10,15 @@ SaveCommand::SaveCommand(const Map& info) :
 
 void SaveCommand::execute() {
     auto doc = Application::getDocument();
-    std::shared_ptr<Serializer> serializer;
+    std::shared_ptr<Serializer> serializer = std::make_shared<Serializer>();
+    auto fileName = defs::toStr(infoMap_["-file"]);
     JSONDocument jsonDoc;
-    serializer->save(doc, jsonDoc);
+    serializer->exec(doc, jsonDoc);
     QByteArray buffer = jsonDoc.toJSON();
-    std::ofstream file(...);
+
+    std::ofstream file(fileName, std::ios::out | std::ios::binary);
     if (file.is_open()) {
-        ///TODO: write to file
+        file.write(buffer.data(), buffer.size());
+        file.close();
     }
 }
